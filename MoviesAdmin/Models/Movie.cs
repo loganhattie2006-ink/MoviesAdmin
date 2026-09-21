2dd45e339fe7eded674d01b90e034879befed601
LoganH-nscc <W0530068@nscc.ca>
2026-09-21 11:31:20 -0300

    Added in a movie class in Models

---
+++ MoviesAdmin/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Runtime { get; set; }
+        
+        public string AgeRating { get; set; }
+
+        public DateTime Released {  get; set; }
+    }
+}
