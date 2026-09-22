592e76f8a307d7dc04c1712e1d43e8da62cb54bc
LoganH-nscc <W0530068@nscc.ca>
2026-09-22 09:15:43 -0300

    Improved movie model(comments, more fields)

--- MoviesAdmin/Models/Movie.cs
+++ MoviesAdmin/Models/Movie.cs
@@ -4,14 +4,20 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
-        public int Runtime { get; set; }
-        
-        public string AgeRating { get; set; }
+        public string AgeRating { get; set; } = string.Empty; // pg-13, R Etc
+
+        public string Genre { get; set; } = string.Empty; //comedy, horror Etc
+
+        public int Runtime { get; set; } // minutes, convert to hours if needed
 
         public DateTime Released {  get; set; }
+
+        public string ImageFileName { get; set; } = string.Empty; // stores the rleated image, probably cover for the films
+
+        public string Director { get; set; } = string.Empty;
     }
 }
